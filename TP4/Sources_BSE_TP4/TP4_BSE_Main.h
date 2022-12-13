afee81345fca049b0c16cbeb7f3dc40c7b83de4c
Maxime Cornaton <maxime.cornaton@sfr.fr>
2022-12-13 20:32:40 +0100

    TP4&5

---
+++ TP4/Sources_BSE_TP4/TP4_BSE_Main.h
@@ -0,0 +1,24 @@
+/*---------------------------------------------------------------------------
+;
+; 	FILE NAME  	: TP2_BSE_Main.H
+; 	TARGET MCUs	: C8051F020, 
+; 	DESCRIPTION	: Configuration pour le TP BSE
+;
+; 	REVISION 1.0
+;
+;---------------------------------------------------------------------------*/
+
+#ifndef __TP2_BSE_Main__
+#define __TP2_BSE_Main__
+
+
+void Config_INT7 (void);
+void Config_INT6 (void);
+void Config_Timer2_TimeBase(void);
+void Modif_Cfg_Globale (void);
+void Oscillator_Init();
+void Config_Timer4_Event_Counter(void);
+void CFG_Clock_UART0(void);
+void CFG_UART0(void);
+char getkey_one_time(void);
+#endif
